2385eba256bc376b40d326a0fb8274183681760c
lx1374327576 <41529712+lx1374327576@users.noreply.github.com>
2019-04-23 18:17:43 +0800

    add data test

---
+++ youtube-VOS/youtubeVOSDataset.py
@@ -0,0 +1,17 @@
+from torch.utils.data.dataset import Dataset
+import torch
+import numpy as np
+import os
+import skimage.io
+
+
+class YoutubeVOSDataset(Dataset):
+
+    def __getitem__(self, item):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __init__(self):
+        pass
